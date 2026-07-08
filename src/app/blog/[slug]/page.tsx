baf6f64efd1127a6ebc178065e3f7be11f4725c2
Derek Wang <derekwang0282@gmail.com>
2026-07-08 10:32:41 -0700

    style(post): move table of contents left

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -95,8 +95,8 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
       </header>
 
       <div className={styles.bodyLayout}>
-        <article className={styles.content}>{renderMarkdown(post.content, tocItems)}</article>
         <PostToc items={tocItems} />
+        <article className={styles.content}>{renderMarkdown(post.content, tocItems)}</article>
       </div>
     </main>
   );
